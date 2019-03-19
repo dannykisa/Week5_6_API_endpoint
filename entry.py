28358c331f0fb7527f063df7b33d8caa147edf9d
Kisakye Winnie <kisawin77@gmail.com>
2019-03-19 15:21:28 -0700

    added some changes

--- entry.py
+++ entry.py
@@ -1,3 +1,18 @@
-from flask import Flask
+class Entry:
+    def __init__(self):
+        self.entries = [{'id': 1, 'entry_name': 'name of the entry', 'anything': 'something'}, {'id': 2, 'entry_name': 'name of the entry', 'anything': 'something'}]
 
-app=Flask(__name__)
+    def all_entries(self):
+        return self.entries
+
+    def single_entry(self, id):
+        for entry in self.entries:
+            if entry['id'] == id:
+                return entry
+            return False
+
+    def create_entry(self, entry):
+        if entry:
+            self.entries.append(entry)
+            return self.entries
+        return 'entry not provided'
